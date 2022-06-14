8291824b813875d6ef9ea4f2fadc98e8b1da41d5
PavloChumak <pashashock@gmail.com>
2022-06-14 13:27:59 -0400

    update Fists

--- src/setup/First.java
+++ src/setup/First.java
@@ -4,5 +4,12 @@ public class First {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+
+        // this is new line
+
+        //some notes......
+
+        // lets try again to git
     }
 }
